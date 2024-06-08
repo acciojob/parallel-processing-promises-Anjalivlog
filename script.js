05b9f1cc71f8fd2a8aff8ad4b07de92bf329cb29
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-06-08 12:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener('click', function() {
+  const promises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      fetch(image.url)
+        .then(response => {
+          if (response.ok) {
+            resolve(image.url);
+          } else {
+            reject(`Failed to load image's URL: ${image.url}`);
+          }
+        })
+        .catch(() => {
+          reject(`Failed to load image's URL: ${image.url}`);
+        });
+    });
+  });
+
+
+ Promise.all(promises)
+    .then(urls => {
+      urls.forEach(url => {
+        const img = document.createElement('img');
+        img.src = url;
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});	
